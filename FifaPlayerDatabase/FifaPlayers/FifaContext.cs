0bee514fc349e027d9a5811e48e331b8898344ab
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-09-12 14:56:34 +0200

    dbContext

---
+++ FifaPlayerDatabase/FifaPlayers/FifaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FifaPlayers
+{
+    class FifaContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = DbFifa;");
+        }
+    }
+}
