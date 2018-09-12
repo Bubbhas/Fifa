160bec678a8d39fbb13de3301532428bdae0d0cd
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-09-12 15:41:27 +0200

    Database

---
+++ FifaPlayerDatabase/FifaPlayers/Migrations/20180912134029_Start.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FifaPlayers.Migrations
+{
+    public partial class Start : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RealTeams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealTeams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTeams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TeamName = table.Column<string>(nullable: true),
+                    TeamMoney = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTeams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FootballPlayers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    UserTeamId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FootballPlayers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FootballPlayers_UserTeams_UserTeamId",
+                        column: x => x.UserTeamId,
+                        principalTable: "UserTeams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTeamPlayers",
+                columns: table => new
+                {
+                    UserTeamId = table.Column<int>(nullable: false),
+                    PlayerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTeamPlayers", x => new { x.PlayerId, x.UserTeamId });
+                    table.ForeignKey(
+                        name: "FK_UserTeamPlayers_FootballPlayers_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "FootballPlayers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserTeamPlayers_UserTeams_UserTeamId",
+                        column: x => x.UserTeamId,
+                        principalTable: "UserTeams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FootballPlayers_UserTeamId",
+                table: "FootballPlayers",
+                column: "UserTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTeamPlayers_UserTeamId",
+                table: "UserTeamPlayers",
+                column: "UserTeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RealTeams");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "UserTeamPlayers");
+
+            migrationBuilder.DropTable(
+                name: "FootballPlayers");
+
+            migrationBuilder.DropTable(
+                name: "UserTeams");
+        }
+    }
+}
