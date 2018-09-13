d08706bcc3e5e4e477f2ca7c1e1f8499d104ae6e
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-09-13 10:47:48 +0200

    ändra

--- FifaPlayerDatabase/FifaPlayers/Menu.cs
+++ FifaPlayerDatabase/FifaPlayers/Menu.cs
@@ -6,26 +6,10 @@ namespace FifaPlayers
     {
         public void StartMenu()
         {
-            ShowNiceArt();
+            
+            
         }
 
-        private void ShowNiceArt()
-        {
-            App app = new App();
-            app.CenterText(@"                              +---------------------------+    ");
-            app.CenterText(@"                              |\                          |\   ");
-            app.CenterText(@"                              | \    @ \_    /            | \");
-            app.CenterText(@"                              |  \  /  \_o--<_/           | o\");
-            app.CenterText(@"______________________________|___|/______________________|-|\|__________________");
-            app.CenterText(@"         /                   /    /              _ o     / /|_                /");
-            app.CenterText(@"        /                   /  _o'------------- / / \ ----/                  /");
-            app.CenterText(@"       /                   /  /|_                /\    /                    /");
-            app.CenterText(@"      /                   /_ /\ _______________ / / __/                    /");
-            app.CenterText(@"     /                      / /                                           /");
-            app.CenterText(@"    /                                                                    /");
-            app.CenterText(@"   /                                                                    /");
-            app.CenterText(@"  /                                                                    /");
-            app.CenterText(@"/____________________________________________________________________/");
-        }
+       
     }
 }
