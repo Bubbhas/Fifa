ff06f8f2a36a786710f4a73ce83fc211eacfe07d
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-09-13 14:09:42 +0200

    Name i player

--- FifaPlayerDatabase/FifaPlayers/Classes/FootballPlayer.cs
+++ FifaPlayerDatabase/FifaPlayers/Classes/FootballPlayer.cs
@@ -9,8 +9,7 @@ namespace FifaPlayers.Classes
     public class FootballPlayer
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
         public decimal Price { get; set; }
         public Position Position { get; set; }
 
