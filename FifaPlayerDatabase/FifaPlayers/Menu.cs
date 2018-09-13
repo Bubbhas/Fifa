b05e2e9f746945925d19bc87c65cc7c36af8ce16
Christoffer J. Öhrström <christoffer.j.ohrstrom@gmail.com>
2018-09-13 10:39:48 +0200

    La till en fin bild hihi

--- FifaPlayerDatabase/FifaPlayers/Menu.cs
+++ FifaPlayerDatabase/FifaPlayers/Menu.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FifaPlayers
 {
@@ -8,7 +6,26 @@ namespace FifaPlayers
     {
         public void StartMenu()
         {
+            ShowNiceArt();
+        }
 
+        private void ShowNiceArt()
+        {
+            App app = new App();
+            app.CenterText(@"                              +---------------------------+    ");
+            app.CenterText(@"                              |\                          |\   ");
+            app.CenterText(@"                              | \    @ \_    /            | \");
+            app.CenterText(@"                              |  \  /  \_o--<_/           | o\");
+            app.CenterText(@"______________________________|___|/______________________|-|\|__________________");
+            app.CenterText(@"         /                   /    /              _ o     / /|_                /");
+            app.CenterText(@"        /                   /  _o'------------- / / \ ----/                  /");
+            app.CenterText(@"       /                   /  /|_                /\    /                    /");
+            app.CenterText(@"      /                   /_ /\ _______________ / / __/                    /");
+            app.CenterText(@"     /                      / /                                           /");
+            app.CenterText(@"    /                                                                    /");
+            app.CenterText(@"   /                                                                    /");
+            app.CenterText(@"  /                                                                    /");
+            app.CenterText(@"/____________________________________________________________________/");
         }
     }
 }
