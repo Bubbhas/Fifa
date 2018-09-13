8340ea5e1ac90e204eea12c02ebd12f5ebbfab96
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-09-13 11:04:33 +0200

    Lagt till meny

--- FifaPlayerDatabase/FifaPlayers/Menu.cs
+++ FifaPlayerDatabase/FifaPlayers/Menu.cs
@@ -4,12 +4,28 @@ namespace FifaPlayers
 {
     public class Menu
     {
+        App app = new App();
         public void StartMenu()
         {
-            
-            
-        }
+            Console.Clear();
+            app.WhiteCenterTextWithoutNewLine("Vad vill du göra?");
 
-       
+            app.CenterText("A) Skapa ditt fantasylag");
+            app.CenterText("B) Ändra i ditt fantasy");
+            app.CenterText("C) Se på ditt fantasylag");
+            app.CenterText("");
+            app.CenterText("");
+            StartMenyChoices(Console.ReadKey().Key);
+
+        }
+       public void StartMenyChoices(ConsoleKey command)
+        {
+            switch (command)
+            {
+                case ConsoleKey.A: Någonmetod: break;
+                case ConsoleKey.B: någonmetod: break;
+                case ConsoleKey.C: någonmetod2: break;
+            }
+        }
     }
 }
