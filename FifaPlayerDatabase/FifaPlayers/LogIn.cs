756705239bf5a717094676f166897a40843c0818
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-09-13 10:04:21 +0200

    samma som förra med en liten liten ändring(jag hade glömt spara innan)

--- FifaPlayerDatabase/FifaPlayers/LogIn.cs
+++ FifaPlayerDatabase/FifaPlayers/LogIn.cs
@@ -14,7 +14,6 @@ namespace FifaPlayers
             Console.WriteLine();
             app.WhiteCenterTextWithoutNewLine("(L)ogga in eller (S)kapa konto?");
             string str = Console.ReadLine().ToLower();
-
             if (str == "l")
             {
                 ShowLogIn();
@@ -38,7 +37,32 @@ namespace FifaPlayers
             app.CenterText("Skapa din profil!\n");
             app.WhiteCenterTextWithoutNewLine("Ange ditt önskade användarnamn: ");
             string username = Console.ReadLine();
-
+            string checkedUsername = CheckValidationOnUsername();
+            string password = "";
+            app.WhiteCenterTextWithoutNewLine("Ange ditt lösenord: ");
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    password = password.Remove(password.Length - 1);
+                    Console.Write("\b \b");
+                }
+                else if (key.Key != ConsoleKey.Enter)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
+            } while (key.Key != ConsoleKey.Enter);
+            bool successfullCreation = dataAccess.CreateProfile(username, password);
+            if(successfullCreation)
+            {
+                Console.Clear();
+                app.CenterText("Ditt konto är skapat");
+                Console.ReadKey();
+                ShowLogIn();
+            }
         }
 
         private void ShowLogIn()
@@ -80,8 +104,6 @@ namespace FifaPlayers
                 Console.ReadKey();
                 ShowLogIn();
             }
-
-
         }
     }
 }
